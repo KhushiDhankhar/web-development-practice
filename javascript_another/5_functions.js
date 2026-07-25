8ea17bbd3e4a6060af853f5a4fae8132e11d3a4e
KhushiDhankhar <khushu915@gmail.com>
2026-07-26 03:43:40 +0530

    added functions

---
+++ javascript_another/5_functions.js
@@ -0,0 +1,27 @@
+// functions
+
+// function functionName(parameters){
+//     // code 
+//     return res;
+// }
+
+
+// table 
+function table(a){
+    for(var i=1;i<=10;i++){
+        console.log(`${a} * ${i} = ${a*i}`);
+    }
+}
+
+table(5);
+
+//  sum
+
+function sum(a,b){
+    var ans = a+b;
+    return ans;
+}
+
+console.log(sum(5,10));
+console.log(sum(15,110));
+console.log(sum(58,108));
