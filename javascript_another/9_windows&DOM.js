b3a2dbeb7ce6e40c02cf463fbe3d801a37a7aa2c
KhushiDhankhar <khushu915@gmail.com>
2026-07-31 23:50:44 +0530

    learning DOM and BOM

---
+++ javascript_another/9_windows&DOM.js
@@ -0,0 +1,75 @@
+/*
+    Window
+
+    1: Window is the main container, or we can say the
+    global Object and any operations related to entire
+    browser window can be a part of window object.
+
+    2: All the members like objects, methods or
+    properties. If they are the part of window object, then
+    we do not refer the window object.
+
+    3:Window has methods, properties and object. ex
+    setTimeout() or setInterval() are the methods, where
+    as Document is the object of the Window and It also
+    has a screen object with properties describing the
+    physical display.
+
+*/
+
+
+/*
+    Document
+
+    1: Whereas the DOM is the child of window object
+
+    2: Where in the DOM we need to refer the document, if
+    we want to use the document object, methods or
+    properties
+
+    3: Document is just the object of the global object that
+    is Window, which deals with the document HTML
+    elements themselves.
+*/
+
+/*
+    Window Object:
+    The window object represents the global window in a browser. Both the
+    Browser Object Model (BOM) and the Document Object Model (DOM) are
+    part of the window object.
+
+    Browser Object Model (BOM): The BOM represents the
+    properties for interacting with the browser itself (not directly
+    related to the content of a web page).
+
+    Examples of BOM features include window. navigator for browser
+    information, window. location for URL manipulation, and window.alert
+    for displaying alerts.
+
+*/
+
+/*
+    Document Object Model (DOM):
+    The DOM represents the structured document as a tree of objects,
+    where each object corresponds to a part of the document (such as
+    elements, attributes, and text).
+
+    The DOM is primarily concerned with the content of the web page
+    allows JavaScript to interact with and manipulate the HTML element
+*/
+
+/*
+    So, while the DOM is focused on the content of the
+    page, the BOM is focused on the browser environment.
+    The window object serves as the global object that
+    encompasses both the BOM and the DOM when working in
+    a browser environment.
+*/
+
+/*
+    The Document Object Model (DOM) is a tree like representation of the HTML
+    document. It provides a way to interact with the HTML document using
+    JavaScriot. The DOM provides multiple properties and methods to dynamically
+    change the content of the HTML document using JavaScript
+
+*/
