83fe38ae2f581256e922e3733aa9e439f5a03e36
KhushiDhankhar <khushu915@gmail.com>
2026-08-24 23:05:52 +0530

    learning date objects

---
+++ javascript_another/date_time_obj.js
@@ -0,0 +1,15 @@
+// date object
+
+// the date() constructor creates Date objects. when calledasa function , it returns a string representing the current time
+
+// synatx:
+
+new Date();
+new Date(date string);
+new Date(year,month);
+new Date(year,month,day);
+new Date(year,month,day,hours);
+new Date(year,month,day,hours,minutes);
+new Date(year,month,day ,hours,minutes , seconds);
+new Date(year,month,day , hours, minutes , seconds, milliseconds);
+new Date(milliseconds);
