42fb5476ef819e7f81700cd59691e70a1a699cc6
KhushiDhankhar <khushu915@gmail.com>
2026-08-25 23:22:28 +0530

    date and time obj

--- javascript_another/date_time_obj.js
+++ javascript_another/date_time_obj.js
@@ -1,15 +1,56 @@
-// date object
 
-// the date() constructor creates Date objects. when calledasa function , it returns a string representing the current time
+// Date & Time Objects in JavaScript
 
-// synatx:
+//Date() constructor: The Date() constructor creates Date objects. when called as a function, 
+// it returns a string representing the current time
 
-new Date();
-new Date(date string);
-new Date(year,month);
-new Date(year,month,day);
-new Date(year,month,day,hours);
-new Date(year,month,day,hours,minutes);
-new Date(year,month,day ,hours,minutes , seconds);
-new Date(year,month,day , hours, minutes , seconds, milliseconds);
-new Date(milliseconds);
+// Creating a Date Object:
+// You can create a new Date object using the new keyword. It can be done in severat ways:
+// todo There are 9 ways to create a new date object:
+
+
+// syntax:
+
+// new Date();
+// new Date(datestring);
+// new Date(year,month);
+// new Date(year,month,day);
+// new Date(year,month,day,hours);
+// new Date(year,month,day,hours,minutes);
+// new Date(year,month,day ,hours,minutes , seconds);
+// new Date(year,month,day , hours, minutes , seconds, milliseconds);
+// new Date(milliseconds);
+
+
+// Current date and time
+
+// new Date(): Creates a Date object representing the current date and time.
+
+    const currentDate = new Date();
+    console.log(currentDate);
+
+//todo This is the ISO 8601 format, which is a standard for representing dates and times.
+// In this format, the date and time are represented together, separated by the letter "T".
+// The "Z" at the end indicates that the time is in UTC (Coordinated Universal Time).
+
+// But the same when you run on browser it will return more human-readable format.
+
+
+// 2: new Date(dateString): Creates a Date object based on the provided date string.
+
+    const dateString = "2024-02-19T10:44:09.274Z";
+    const dateFromString = new Date(dateString);
+    console.log(dateFromString);
+
+// 3: new Date(year, month): Creates a Date object with the specified year and month.
+
+    // const date1 = new Date(2024, 1);
+    // console.log(date1);
+
+// 4: new Date(year, month, day): Creates a Date object with the specified year, mon
+// and day.
+
+    // const date2 = new Date(2024, 1,19);
+    // console.log(date2);
+
+// 5: new Date(year, month, day, hours): Creates a Date object with the specified y
